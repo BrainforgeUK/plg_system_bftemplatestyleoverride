5b2f81fc8bc4f34f1f7f0408a10cbe331ea45ee2
Brainforge.UK <brainforge.uk@btinternet.com>
2022-08-31 08:44:47 +0100

    Joomla 4 changes and added template style css/js customisation feature.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-This plugin enables you use a URL parameter on your home
-page to override the default template.<br/>This feature is often useful when demonstrating proposed
-template changes.<br/></br>
+This plugin provides the following features:
+<ul>
+<li> Enables you use a URL parameter on your home page to override the default template.<br/>
+This feature is often useful when demonstrating proposed template / style changes.<br/></br>
 Example useage (using the default parameter name):<br/>
-https://mysite.com?bftemplatestyleid=5
+https://mysite.com?bftemplatestyleid=5<br/>
+</li>
+<li>
+Provides access to template style specific CSS and JS files (if they exist).
+The names of these files will be...
+<ul>
+<li>media/templates/mytemplate/css/mytemplatestylealias.css</li>
+<li>media/templates/mytemplate/css/mytemplatestylealias.js</li>
+</ul>
+The alias <i>mytemplatestyle</i> is generated from the template style title (i.e. lower case and all
+non-alphabetic characters replaced with a hyphen (-)). This is follows the same rules as used for
+article and menu alias generation.
+</li>
+</ul>
+You don't have to use both features, they function independently.
--- bftemplatestyleoverride.php
+++ bftemplatestyleoverride.php
@@ -1,13 +1,16 @@
 <?php
 /**
- * @package   HikaShop for Joomla!
+ * @package   Template style override plugin by Brainforge
  * @author    https://www.brainforge.co.uk
- * @copyright (C) 2020 Jonathan Brain. All rights reserved.
+ * @copyright (C) 2020-2022 Jonathan Brain. All rights reserved.
  * @license   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Filter\OutputFilter;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Component\Templates\Administrator\Model\StyleModel;
 use Joomla\Registry\Registry;
 
 defined('_JEXEC') or die('Restricted access');
@@ -16,31 +19,40 @@ class plgSystemBftemplatestyleoverride extends CMSPlugin
 {
 	const TEMPLATESTYLEOVERIDESTATE = "Bftemplatestyleoverride.template";
 
+	protected $app;
+	protected $params;
+
+	/*
+	 */
 	public function __construct(&$subject, $config) {
 		parent::__construct($subject, $config);
+
+		// TODO Needed?
+		//$this->app = Factory::getApplication();
 	}
 
+	/*
+	 */
 	public function onAfterRoute() {
-		$app = Factory::getApplication();
-		if (!$app->isClient('site')) {
+		if (!$this->app->isClient('site')) {
 			return;
 		}
 
 		// Get the id of the active menu item
-		$menu = $app->getMenu();
+		$menu = $this->app->getMenu();
 		$item = $menu->getActive();
 		$template = null;
 
 		if (!$item) {
-			$item = $menu->getItem($app->input->getInt('Itemid', null));
+			$item = $menu->getItem($this->app->input->getInt('Itemid', null));
 		}
 		$isHome = ($item && $item->home);
 
-		$tid = $app->input->getUint($this->params->get('paramname', 'bftemplatestyleid'), null);
+		$tid = $this->app->input->getUint($this->params->get('paramname', 'bftemplatestyleid'), null);
 
 		if ($tid) {
 			// Load style
-			$db = \JFactory::getDbo();
+			$db = Factory::getDbo();
 			$query = $db->getQuery(true)
 				->select('id, home, template, s.params')
 				->from('#__template_styles as s')
@@ -61,17 +73,32 @@ class plgSystemBftemplatestyleoverride extends CMSPlugin
 				$template->params->set('styleid', $tid);
 			}
 
-			$app->setUserState(self::TEMPLATESTYLEOVERIDESTATE, $template);
+			$this->app->setUserState(self::TEMPLATESTYLEOVERIDESTATE, $template);
 		}
 		else if (!$isHome) {
-			$template = $app->getUserState(self::TEMPLATESTYLEOVERIDESTATE, $template);
+			$template = $this->app->getUserState(self::TEMPLATESTYLEOVERIDESTATE, $template);
 		}
 		else {
-			$app->setUserState(self::TEMPLATESTYLEOVERIDESTATE, null);
+			$this->app->setUserState(self::TEMPLATESTYLEOVERIDESTATE, null);
 		}
 
 		if (!empty($template)) {
-			$app->setTemplate($template->template, $template->params);
+			$this->app->setTemplate($template->template, $template->params);
 		}
+	}
+
+	/**
+	 */
+	public function onBeforeRender()
+	{
+		$template = $this->app->getTemplate(true);
+		$styleModel = new StyleModel();
+		$style = $styleModel->getItem($template->id);
+		$style->alias = OutputFilter::stringURLSafe($style->title);
+
+		$base = Uri::base() .  'media/templates/' . $this->template;
+		$doc = Factory::getDocument();
+		$doc->addStyleSheet($base . '/css/' . $style->alias . '.css');
+		$doc->addScript($base . '/js/' . $style->alias . '.js', 'text/javascript');
 	}
 }
